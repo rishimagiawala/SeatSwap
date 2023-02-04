ad73fd0452f226a082737a3d6d8e9606a570d8cc
saurinya <rishimagiawala@gmail.com>
2023-02-04 09:24:03 -0500

    Made Navigator

--- App.js
+++ App.js
@@ -2,24 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image} from 'react-native';
 import { useTheme, Button, Appbar, } from 'react-native-paper';
 import { Provider as PaperProvider } from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TitleAppbar from './components/TitleAppbar'
 import AirlineDropDown from './components/AirlineDropdown';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator()
 export default function App() {
   const theme = useTheme();
   
   return (
-    <PaperProvider>
-    <SafeAreaProvider>
-      
-    <View style={{backgroundColor: theme.colors.primary}}>
-      <TitleAppbar title = "Welcome to My App" yogith="Hello" />
-      <AirlineDropDown select= "Select a Gender" />
-    </View>
-    </SafeAreaProvider>
-    </PaperProvider>
+   <NavigationContainer>
+    
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SSODelta"
+          component={SSODelta}
+          options={{title: 'Sign In to Delta'}}
+        />
+       
+        
+        
+      </Stack.Navigator>
+    
+    
+    </NavigationContainer>
+    
   );
 }
 
