4898d5c7af856212ab44a3863a078002c87c7a3d
Manav Patel <92553242+manavpatelUGA@users.noreply.github.com>
2023-02-04 10:59:46 -0500

    added firebaseConfig file

---
+++ firebaseConfig.js
@@ -0,0 +1,29 @@
+firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyA50FO3bt1bCSvDHZ3wvU6_0F9bn_PfBq4",
+    authDomain: "seatswapuga.firebaseapp.com",
+    databaseURL: "https://seatswapuga-default-rtdb.firebaseio.com",
+    projectId: "seatswapuga",
+    storageBucket: "seatswapuga.appspot.com",
+    messagingSenderId: "433869449365",
+    appId: "1:433869449365:web:7f6db6fb179eae6f09939c",
+    measurementId: "G-VJ47NVMM4C"
+  };
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
